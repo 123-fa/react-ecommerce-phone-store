006691f758b24518116c66b63563dfb2b422ef02
123-fa <orafufrancis22@gmail.com>
2023-02-08 17:10:08 -0800

    phone project

---
+++ src/Data.js
@@ -0,0 +1,119 @@
+
+export const storeProducts = [
+    {
+        id: 1,
+        title : "Iphone 11 promax",
+        img: "/images/11pro.jpg",
+        price: 10,
+        company: "APPLE ",
+        info: 
+          "Lorem ipsum dolor amet offal butcher quicna  Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+          inCart: false,
+          count: 0,
+          total: 0   
+    },
+
+    {
+        id: 2,
+        title : "Iphone 13 promax.",
+        img: "images/13promax.jpg",
+        price: 16,
+        company: "APPLE ",
+        info: 
+          "Lorem ipsum dolor amet offal butcher quicna  Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+          inCart: false,
+          count: 0,
+          total: 0   
+    },
+
+    {
+  id: 3,
+        title : "Iphone 14 promax",
+        img: "images/14promax.jpg",
+        price: 10,
+        company: "APPLE ",
+        info: 
+          "Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+          inCart: false,
+          count: 0,
+          total: 0   
+    },
+    {
+      id: 4,
+      title : "Iphone xr",
+      img: "images/iphonexr.jpg",
+      price: 10,
+      company: "APPLE ",
+      info: 
+        "Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+        inCart: false,
+        count: 0,
+        total: 0   
+  },
+ 
+
+    {
+        id: 5,
+        title : "Samsung s10+",
+        img: "images/s10.jpg",
+        price: 10,
+        company: "SAMSUNG ",
+        info: 
+          "Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+          inCart: false,
+          count: 0,
+          total: 0   
+    },
+
+    {
+        id: 6,
+        title : "Samsung S22ultra",
+        img: "images/s22ultra.jpg",
+        price: 10,
+        company: "SAMSUNG ",
+        info: 
+          "Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+          inCart: false,
+          count: 0,
+          total: 0   
+    },
+
+    {
+      id: 7,
+      title : "Infinix note10pro",
+      img: "images/note10pro.jpg",
+      price: 10,
+      company: "INFINIX",
+      info: 
+        "Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+        inCart: false,
+        count: 0,
+        total: 0   
+  },
+
+    {
+        id: 8,
+        title : "Tecnospark8pro",
+        img: "images/tecnospark8pro.jpg",
+        price: 10,
+        company: "TECNO",
+        info: 
+          "Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " , 
+          inCart: false,
+          count: 0,
+          total: 0   
+    }
+];
+
+export const detailProduct = {
+    id: 1,
+    title : "Opporeno4",
+    img: "images/opporeno4.jpg",
+    price: 10,
+    company: "OPPO",
+    info: 
+      "Lorem ipsum dolor amet offal butcher quicna  Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna Lorem ipsum dolor amet offal butcher quicna " ,
+      inCart: false,
+      count: 0,
+      total: 0 ,
+}
